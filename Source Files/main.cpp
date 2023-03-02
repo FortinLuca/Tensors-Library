73a50dff126df8a8b009f8d64b1c729d423b9b37
Luca Fortin <fortluca96@gmail.com>
2023-03-02 14:57:48 +0100

    Create main.cpp
    creazione main

---
+++ Source Files/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+class Solve {
+    public:
+        void solve() {
+            std::cout << "We're going to solve every tensor!" << std::endl;
+        }
+};
+
+int main() {
+    Solve g;
+    g.solve();
+    return 0;
+}
