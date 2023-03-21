000e4382e776adc0524baa7df2ec8c7fff571f77
settimoluca99 <settimoluca99@gmail.com>
2023-03-21 17:24:48 +0100

    Initialization #2

--- Source Files/main.cpp
+++ Source Files/main.cpp
@@ -10,9 +10,27 @@ class Solve {
 };
 
 int main() {
-    vector<int> test = {3, 4, 5};
-    //RankedTensor<float, 3> z(test);
-    RankedTensor<float, 5> rt(1, 2, 3, 4, 5);
+    vector<int> testvector = {1, 2, 3, 4, 5};
+    
+    RankedTensor<float, 5> rt1(testvector);
+    RankedTensor<float, 5> rt2(1, 2, 3, 4, 5);
+
+    int * sizeDimensions1 = rt1.getSizeDimensions();
+    int * sizeDimensions2 = rt2.getSizeDimensions();
+
+    int * strides1 = rt1.getStrides();
+    int * strides2 = rt2.getStrides();
+
+    for(int i=0; i<5; i++){
+        cout<<to_string(sizeDimensions1[i]) + " "+ to_string(sizeDimensions2[i])<<endl;
+    }
+
+    cout<<endl;
+
+    for(int i=0; i<5; i++){
+        cout<<to_string(strides1[i]) + " "+ to_string(strides2[i])<<endl;
+    }
+
 
     return 0;
 }
