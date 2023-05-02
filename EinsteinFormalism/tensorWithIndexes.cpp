579ceff79f6d38b79b3d8c1b7f3baa65cc1ba6a0
settimoluca99 <settimoluca99@gmail.com>
2023-05-02 13:04:21 +0200

    TensorWithIndexes #2

--- EinsteinFormalism/tensorWithIndexes.cpp
+++ EinsteinFormalism/tensorWithIndexes.cpp
@@ -3,13 +3,20 @@
 namespace TensorIndexes{
 
     template <typename T>
-    TensorWithIndexes<T>::TensorWithIndexes(vector<Index> spaces){
+    TensorWithIndexes<T>::TensorWithIndexes(vector<Index> inputSpaces){
+        int sizeInputSpaces = inputSpaces.size();
 
+        if(rank != sizeInputSpaces)
+            throw invalid_argument("The rank must corresponds to the number of indexes inserted in input");
+        
+        for (int i = 0; i < rank; i++){
+            spaces[i] = inputSpaces[i];
+        }
     }
 
     template <typename T>
     template <typename... indexes>
-    TensorWithIndexes<T>::TensorWithIndexes(indexes... space) : TensorWithIndexes<T>::TensorWithIndexes(vector<Index> spaces){
+    TensorWithIndexes<T>::TensorWithIndexes(indexes... inputSpaces) : TensorWithIndexes<T>::TensorWithIndexes({inputSpaces}){
 
     }
 
